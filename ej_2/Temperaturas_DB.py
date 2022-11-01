9fd3567bdb8f405424933012369f4dafa50f349d
FelipeCarrozzo <felipe.carrozzo@yahoo.com>
2022-11-01 09:21:55 -0300

    ya estan todos los metodos del ej2

--- ej_2/Temperaturas_DB.py
+++ ej_2/Temperaturas_DB.py
@@ -5,9 +5,11 @@ Created on Thu Oct 20 11:36:21 2022
 @author: alumno
 """
 
-from ej_2.clases import ArbolAVL, NodoArbol 
-from datetime import datetime 
+from ej_2.clases import NodoArbol 
+from ej_2.clases import ArbolAVL
 from ej_2.clases import Iterador
+from datetime import datetime 
+
 
 #%%
 
@@ -27,74 +29,91 @@ class Temperaturas_DB:
         return str(self.mediciones)
 
 #%%        
-    """Método para mostrar la totalidad de temperaturas 
-    medidas entre una fecha y otra."""
-
-    def mostrar_temperaturas_rango(self,fecha1,fecha2):
-        f_uno = datetime.strptime(fecha1, "%Y/%m/%d")
-        f_dos = datetime.strptime(fecha2, "%Y/%m/%d")
-        Iter = Iterador(self.mediciones,f_uno)
-        lista=[]
-        for nodo in Iter:
-            if f_uno <= nodo.clave <= f_dos:
-                lista.append([str(nodo.clave.date()), nodo.carga_util])
-        print(lista)
-
 
     def guardar_temperatura(self,temperatura,fecha):
-        convertir_fecha = datetime.strptime(fecha,"%Y/%m/%d")
+        convertir_fecha = datetime.strptime(fecha,"%d/%m/%Y")
         self.mediciones.agregar(convertir_fecha,temperatura)
 
         
     def devolver_temperatura(self,fecha):
-        convertir_fecha = datetime.strptime(fecha,"%Y/%m/%d")
-        self.mediciones.obtener(convertir_fecha)
+        convertir_fecha = datetime.strptime(fecha,"%d/%m/%Y")
+        temp = self.mediciones.obtener(convertir_fecha)
+        return temp
         
         
     def max_temp_rango(self,fecha1, fecha2):
-        f_uno = datetime.strptime(fecha1, "%Y/%m/%d")
-        f_dos = datetime.strptime(fecha2, "%Y/%m/%d")
+        f_uno = datetime.strptime(fecha1, "%d/%m/%Y")
+        f_dos = datetime.strptime(fecha2, "%d/%m/%Y")
         temp_max = self.mediciones.obtener(f_uno)
-        ITERADOR = Iterador(self.mediciones, f_dos)
-        
+        ITERADOR = Iterador(self.mediciones, f_uno)
+        l_temp = []
+
         for i in ITERADOR:
-            if f_uno <= NodoArbol.clave <= f_dos:
-                if NodoArbol.carga_util > temp_max:
-                    temp_max = NodoArbol.carga_util
+            if f_uno <= i.clave <= f_dos:
+                conversion = [str(i.clave.date()), i.carga_util]
+                l_temp.append(conversion)
+                if i.carga_util > temp_max:
+                    temp_max = i.carga_util
         return temp_max
     
     def min_temp_rango(self,fecha1, fecha2):
-        f_uno = datetime.strptime(fecha1, "%Y/%m/%d")
-        f_dos = datetime.strptime(fecha2, "%Y/%m/%d")
+        f_uno = datetime.strptime(fecha1, "%d/%m/%Y")
+        f_dos = datetime.strptime(fecha2, "%d/%m/%Y")
         temp_max = self.mediciones.obtener(f_uno)
-        ITERADOR = Iterador(self.mediciones, f_dos)
+        Iter = Iterador(self.mediciones, f_uno)
         l_temp = []
-        for i in ITERADOR:
-            if f_uno >= NodoArbol.clave >= f_dos:
-                conversion = [str(NodoArbol.clave.date()), NodoArbol.carga_util]
+        for i in Iter:
+            if f_uno <= i.clave <= f_dos:
+                conversion = [str(i.clave.date()), i.carga_util]
                 l_temp.append(conversion)
-                if NodoArbol.carga_util < temp_max:
-                    temp_max = NodoArbol.carga_util
+                if i.carga_util < temp_max:
+                    temp_max = i.carga_util
         return temp_max
     
+    def temp_extremos_rango(self,fecha1,fecha2):
+        f_uno = datetime.strptime(fecha1, "%d/%m/%Y")
+        f_dos = datetime.strptime(fecha2, "%d/%m/%Y")
+        
+        
+    def borrar_temperatura(self,fecha):
+        self.mediciones.remover(fecha) 
     
-
+    """Método para mostrar la totalidad de temperaturas 
+    medidas entre una fecha y otra."""
+    def mostrar_temperaturas_rango(self,fecha1,fecha2):
+        f_uno = datetime.strptime(fecha1, "%d/%m/%Y")
+        f_dos = datetime.strptime(fecha2, "%d/%m/%Y")
+        Iter = Iterador(self.mediciones,f_uno)
+        lista=[]
+        for i in Iter:
+            if f_uno <= i.clave <= f_dos:
+                lista.append(i.carga_util)
+        print(lista)
+    
+    
+    def mostrar_cantidad_muestras(self):
+        cantidad = self.mediciones.tamano
+        return cantidad
         
-        
-
 
 if __name__ == "__main__":
     obj=Temperaturas_DB()
-    obj.guardar_temperatura(30,"2022/10/20")
-    obj.guardar_temperatura(27,"2022/10/21")
-    obj.guardar_temperatura(26,"2022/10/22")
-    obj.guardar_temperatura(25,"2022/10/23")
-    obj.guardar_temperatura(23,"2022/10/24")
-    obj.guardar_temperatura(15,"2022/10/25")
+    obj.guardar_temperatura(30,"20/10/2022")
+    obj.guardar_temperatura(27,"21/10/2022")
+    obj.guardar_temperatura(26,"22/10/2022")
+    obj.guardar_temperatura(25,"23/10/2022")
+    obj.guardar_temperatura(23,"24/10/2022")
+    obj.guardar_temperatura(15,"25/10/2022")
+    obj.guardar_temperatura(12,"26/10/2022")
+    obj.guardar_temperatura(9,"27/10/2022")
+    obj.guardar_temperatura(3,"28/10/2022")
+    print(obj.devolver_temperatura("25/10/2022"))
+    print()
+    print("MAX",obj.max_temp_rango("20/10/2022", "28/10/2022"))
+    print("MIN",obj.min_temp_rango("20/10/2022", "28/10/2022")  )  
+    print(obj.borrar_temperatura("25/10/2022"))
+    
+    
     # (obj.mostrar_temperaturas_rango("2022/10/20", "2022/10/25"))
-    print(obj.devolver_temperatura("2022/10/24")) #correjir delvolver_temperatura 
     
-    print(obj.max_temp_rango("2022/10/24", "2022/10/20"))
- 
-    
-    
+    # print(obj.mostrar_cantidad_muestras())
