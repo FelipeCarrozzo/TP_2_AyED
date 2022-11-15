64fd4cbd9fb705b3bf0f6354450ff471f7fd633f
FelipeCarrozzo <felipecarrozzo@yahoo.com>
2022-11-15 14:28:41 -0300

    docstrings

--- Ej_1/modulos/main.py
+++ Ej_1/modulos/main.py
@@ -12,7 +12,9 @@ n = 20  # cantidad de ciclos de simulación
 
 cola_de_espera = mon.MonticuloBinario()
 
-# Ciclo que gestiona la simulación
+"""
+Ciclo que gestiona la simulación
+"""
 for i in range(n):
     # Fecha y hora de entrada de un paciente
     ahora = datetime.datetime.now()
@@ -20,13 +22,17 @@ for i in range(n):
     print('-*-'*15)
     print('\n', fecha_y_hora, '\n')
 
-    # Se crea un paciente un paciente por segundo
-    # La criticidad del paciente es aleatoria
+    """
+    Se crea un paciente por segundo
+    La criticidad del paciente es aleatoria
+    """
     paciente = pac.Paciente(i)
     cola_de_espera.insertar(paciente)
     
 
-    # Atención de paciente en este ciclo: en el 50% de los casos
+    """
+    Atención de paciente en este ciclo: en el 50% de los casos
+    """
     if random.random() < 0.5:
         # se atiende paciente que se encuentra al frente de la cola
         paciente_atendido = cola_de_espera.eliminar_min()
@@ -39,7 +45,9 @@ for i in range(n):
     
     print()
 
-    # Se muestran los pacientes restantes en la cola de espera
+    """
+    Se muestran los pacientes restantes en la cola de espera
+    """
     print('Pacientes que faltan atenderse:', len(cola_de_espera))
     for paciente in cola_de_espera:
         print(paciente)
--- Ej_1/modulos/paciente.py
+++ Ej_1/modulos/paciente.py
@@ -8,20 +8,45 @@ descripciones_de_riesgo = ['crítico', 'moderado', 'bajo']
 # probabilidades de aparición de cada tipo de paciente
 probabilidades = [0.1, 0.3, 0.6] 
 
+
 class Paciente:
+    """Se define una clase Paciente"""    
     def __init__(self,orden):
+        """
+        
+        Parameters
+        ----------
+        orden : TYPE int
+            El constructor de la clase Paciente guarda nombres, apellidos,
+            las probabilidades de cada nivel de riesgo, la descripción y el 
+            orden
+            
+        Returns
+        -------
+        None.
+
+        """
         n = len(nombres)
         self._nombre = nombres[randint(0, n-1)]
         self._apellido = apellidos[randint(0, n-1)]
-        self._riesgo = choices(niveles_de_riesgo, probabilidades)[0]
+        self._riesgo = choices(niveles_de_riesgo, probabilidades)[0] #retorna los niveles de riesgo según sus probabilidades
         self._descripcion = descripciones_de_riesgo[self._riesgo-1]
         self._orden = orden
         
     def __str__(self):
+          """
+        Returns
+        -------
+        cad : TYPE any
+            Método mágico que imprime por consola los datos personales de los 
+            pacientes: nombre, apellido, nivel de riesgo y descripición.
+
+        """
           cad = self._nombre + ' '
           cad += self._apellido + '\t -> '
           cad += str(self._riesgo) + '-' + self._descripcion
           return cad    
+        
         
     def __lt__(self,other):
         if self._riesgo == other._riesgo:
